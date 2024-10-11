111d1099f4928545251f07d05dc564f2af4ce13a
Zhikai Cai <imfeh2@gmail.com>
2024-10-11 14:54:04 +0800

    feat: scalable window

---
+++ src/core/screenshot.rs
@@ -0,0 +1,38 @@
+use crate::utils::image;
+use windows::Win32::Foundation::HWND;
+use windows::Win32::Graphics::Gdi::{BitBlt, CreateCompatibleBitmap, CreateCompatibleDC, DeleteDC, DeleteObject, GetDC, SelectObject, HBITMAP, SRCCOPY};
+use windows::Win32::UI::WindowsAndMessaging::GetDesktopWindow;
+
+pub struct Screenshot;
+
+impl Screenshot {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn shot(&self, x1: i32, y1: i32, x2: i32, y2: i32) -> image::ImageSaver {
+        unsafe {
+            let hwnd: HWND = GetDesktopWindow();
+            let hdc = GetDC(hwnd);
+            let hdc_mem = CreateCompatibleDC(hdc);
+
+            assert!(x1 < x2 && y1 < y2, "Invalid coordinates");
+
+            let width = x2 - x1;
+            let height = y2 - y1;
+
+            let hbm: HBITMAP = CreateCompatibleBitmap(hdc, width, height);
+            SelectObject(hdc_mem, hbm);
+
+            BitBlt(hdc_mem, 0, 0, width, height, hdc, x1, y1, SRCCOPY).expect("BitBlt failed");
+
+            let image = image::ImageSaver::from_bitmap(hdc_mem, width as u32, height as u32);
+
+            let _ = DeleteObject(hbm);
+            let _ = DeleteDC(hdc_mem);
+            let _ = DeleteDC(hdc);
+
+            image
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,29 @@
+mod core {
+    pub mod screenshot;
+}
+
 mod utils {
     pub mod image;
 }
 
-use crate::utils::image;
-use windows::Win32::Foundation::HWND;
-use windows::Win32::Graphics::Gdi::{BitBlt, CreateCompatibleBitmap, CreateCompatibleDC, DeleteDC, DeleteObject, GetDC, SelectObject, HBITMAP, SRCCOPY};
+use crate::core::screenshot::Screenshot;
 use windows::Win32::UI::HiDpi::{SetProcessDpiAwarenessContext, DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2};
-use windows::Win32::UI::WindowsAndMessaging::{GetDesktopWindow, GetSystemMetrics, SM_CXSCREEN, SM_CYSCREEN};
+use windows::Win32::UI::WindowsAndMessaging::{GetSystemMetrics, SM_CXSCREEN, SM_CYSCREEN};
 
-fn main() {
+fn init() {
     unsafe {
         SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2).expect("Failed to SetProcessDpiAwarenessContext");
     }
+}
 
-    let hwnd: HWND = unsafe { GetDesktopWindow() };
-    let hdc = unsafe { GetDC(hwnd) };
-    let hdc_mem = unsafe { CreateCompatibleDC(hdc) };
-
-    let width = unsafe { GetSystemMetrics(SM_CXSCREEN) };
-    let height = unsafe { GetSystemMetrics(SM_CYSCREEN) };
-
-    let hbm: HBITMAP = unsafe { CreateCompatibleBitmap(hdc, width, height) };
-    unsafe { SelectObject(hdc_mem, hbm) };
+fn main() {
+    init();
 
     unsafe {
-        BitBlt(hdc_mem, 0, 0, width, height, hdc, 0, 0, SRCCOPY).expect("BitBlt failed");
-    }
-
-    let image_saver = image::ImageSaver::from_bitmap(hdc_mem, width as u32, height as u32);
-    match image_saver.save("screenshot.png") {
-        Ok(_) => println!("Screenshot saved"),
-        Err(e) => println!("Failed to save screenshot: {:?}", e),
-    }
-
-
-    unsafe {
-        let _ = DeleteObject(hbm);
-        let _ = DeleteDC(hdc_mem);
-        let _ = DeleteDC(hdc);
+        let width = GetSystemMetrics(SM_CXSCREEN);
+        let height = GetSystemMetrics(SM_CYSCREEN);
+        let screenshot = Screenshot::new();
+        let image = screenshot.shot(0, 0, width, height);
+        image.save("screenshot.png").expect("Failed to save image");
     }
 }
