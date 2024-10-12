4ed697360c8a9f09dd2c6954ec6179ca9ed493d2
Zhikai Cai <imfeh2@gmail.com>
2024-10-12 16:45:10 +0800

    refactor: remove `Screenshot::new()`

--- src/core/screenshot.rs
+++ src/core/screenshot.rs
@@ -1,4 +1,5 @@
 use crate::utils::image::Image;
+use crate::utils::structures::Rectangle;
 use windows::Win32::Foundation::HWND;
 use windows::Win32::Graphics::Gdi::{BitBlt, CreateCompatibleBitmap, CreateCompatibleDC, DeleteDC, DeleteObject, GetDC, SelectObject, HBITMAP, SRCCOPY};
 use windows::Win32::UI::WindowsAndMessaging::GetDesktopWindow;
@@ -6,27 +7,18 @@ use windows::Win32::UI::WindowsAndMessaging::GetDesktopWindow;
 pub struct Screenshot;
 
 impl Screenshot {
-    pub fn new() -> Self {
-        Self
-    }
-
-    pub fn shot(&self, x1: i32, y1: i32, x2: i32, y2: i32) -> Image {
+    pub fn shot(&self, rect: &Rectangle) -> Image {
         unsafe {
             let hwnd: HWND = GetDesktopWindow();
             let hdc = GetDC(hwnd);
             let hdc_mem = CreateCompatibleDC(hdc);
 
-            assert!(x1 < x2 && y1 < y2, "Invalid coordinates");
-
-            let width = x2 - x1;
-            let height = y2 - y1;
-
-            let hbm: HBITMAP = CreateCompatibleBitmap(hdc, width, height);
+            let hbm: HBITMAP = CreateCompatibleBitmap(hdc, rect.width() as i32, rect.height() as i32);
             SelectObject(hdc_mem, hbm);
 
-            BitBlt(hdc_mem, 0, 0, width, height, hdc, x1, y1, SRCCOPY).expect("BitBlt failed");
+            BitBlt(hdc_mem, 0, 0, rect.width() as i32, rect.height() as i32, hdc, rect.left, rect.top, SRCCOPY).expect("BitBlt failed");
 
-            let image = Image::from_bitmap(hdc_mem, width as u32, height as u32);
+            let image = Image::from_bitmap(hdc_mem, rect.dimension());
 
             let _ = DeleteObject(hbm);
             let _ = DeleteDC(hdc_mem);
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,31 @@ mod core {
 
 mod utils {
     pub mod image;
+    pub mod structures;
 }
 
 use crate::core::screenshot::Screenshot;
+use crate::utils::structures::{Rectangle, FULLSCREEN};
 use windows::Win32::UI::HiDpi::{SetProcessDpiAwarenessContext, DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2};
 use windows::Win32::UI::WindowsAndMessaging::{GetSystemMetrics, SM_CXSCREEN, SM_CYSCREEN};
 
 fn init() {
     unsafe {
         SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2).expect("Failed to SetProcessDpiAwarenessContext");
+        FULLSCREEN.right = GetSystemMetrics(SM_CXSCREEN);
+        FULLSCREEN.bottom = GetSystemMetrics(SM_CYSCREEN);
     }
 }
+
 
 fn main() {
     init();
 
     unsafe {
-        let width = GetSystemMetrics(SM_CXSCREEN);
-        let height = GetSystemMetrics(SM_CYSCREEN);
+        let rect = Rectangle::new(100, 50, 2400, 1600);
+
         let screenshot = Screenshot::new();
-        let image = screenshot.shot(0, 0, width, height);
+        let image = screenshot.shot(&rect);
         image.save("screenshot.png").expect("Failed to save image");
     }
 }
